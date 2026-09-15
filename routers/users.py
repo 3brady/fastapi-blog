80c9023e89285a45dfba3d0186a04262e73e4925
ahmed aly abdelrady <22abdelrady22@gmail.com>
2026-09-15 16:29:54 +0300

    added js forms

--- routers/users.py
+++ routers/users.py
@@ -34,7 +34,7 @@ async def get_user_post(user_id: int, db: Annotated[AsyncSession, Depends(get_db
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
 
     result = await db.execute(
-        select(models.Post).options(selectinload(models.Post.author)).where(models.Post.user_id == user_id))
+        select(models.Post).options(selectinload(models.Post.author)).where(models.Post.user_id == user_id).order_by(models.Post.date_posted.desc()))
     posts = result.scalars().all()
     return posts
 
